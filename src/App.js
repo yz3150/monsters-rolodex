a180ee33d5ac2d46e3da9b1b9e9834fc614cbb23
Yanting Zhong <yz3150@tc.columbia.edu>
2019-10-30 20:42:05 -0400

    create searchBox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -17,10 +19,23 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monsters =>
+      monsters.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search for monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
